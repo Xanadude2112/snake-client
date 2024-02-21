38d8d2a2664efdd0471e4219f4bcf0589b2ea1a5
Thomas Azran <tommy.azran@gmail.com>
2024-02-21 18:17:14 +0000

    implementing movement

--- play.js
+++ play.js
@@ -1,1 +1,25 @@
-const {client} = require('./client');
+const {connect} = require('./client');
+
+// setup interface to handle user input from stdin
+let connection;
+
+const handleUserInput = (key) => {
+  if(key === `\u0003`) {
+    process.exit();
+  }
+  if(key === 'w' || key === 'W') connection.write(`Move: up`);
+  if(key === 'd' || key === 'D') connection.write(`Move: right`);
+  if(key === 's' || key === 'S') connection.write(`Move: down`);
+  if(key === 'a' || key === 'A') connection.write(`Move: left`);
+}
+
+const setupInput = function () {
+  connection = connect();
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput)
+  stdin.resume();
+  return stdin;
+};
+setupInput();
