9b896875fda67e1176294ddd6cfafe94d62853fc
Thomas Azran <tommy.azran@gmail.com>
2024-02-21 19:49:32 +0000

    refactored

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const {connect} = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// setup interface to handle user input from stdin
-let connection;
-
-const handleUserInput = (key) => {
-  if(key === `\u0003`) {
-    process.exit();
-  }
-  if(key === 'w' || key === 'W') connection.write(`Move: up`);
-  if(key === 'd' || key === 'D') connection.write(`Move: right`);
-  if(key === 's' || key === 'S') connection.write(`Move: down`);
-  if(key === 'a' || key === 'A') connection.write(`Move: left`);
-}
-
-const setupInput = function () {
-  connection = connect();
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput)
-  stdin.resume();
-  return stdin;
-};
-setupInput();
+console.log("Connecting ...");
+const resultConnection = connect();
+setupInput(resultConnection);
